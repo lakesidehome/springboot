07d87fc602dee77cad4870c197e4fd46efeb76a9
liuxh <xiaohuan.liu@gh-ca.com>
2022-11-19 17:52:40 +0800

    'springboot初始化'

---
+++ src/main/java/com/example/springbootdemo1/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springbootdemo1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    /**
+     * 请求 /hello  输出hello springboot!
+     * @return
+     */
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return "hello springboot! demo2!";
+    }
+}
